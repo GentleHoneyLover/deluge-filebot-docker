7c3f476957ae2620b433891d5de2e8ca4d3f22dc
Honey Lover <38346023+GentleHoneyLover@users.noreply.github.com>
2022-01-07 12:09:38 +0100

    Removed init scrip logging. docker logs can be used instead

--- init.sh
+++ init.sh
@@ -1,29 +1,26 @@
 #!/bin/sh
 
-# Setup a log file
-LOG="/config/init.log"
-
-echo "---" >> $LOG
-echo "$(date +%d.%m.%Y\ %H:%M:%S) - Starting the container..." | tee -a $LOG
+echo "$(date +%d.%m.%Y\ %H:%M:%S) - Starting the container..."
 
 # Calling out user-defined environment variables
-echo "Grabbing PUID=${PUID} and PGID=${PGID}..." | tee -a $LOG
-echo "Grabbing UMASK=${UMASK}..." | tee -a $LOG
-echo "Grabbing DELUGE_LOGLEVEL=${DELUGE_LOGLEVEL}..." | tee -a $LOG
+echo "Grabbing PUID=${PUID} and PGID=${PGID}..."
+echo "Grabbing UMASK=${UMASK}..."
+echo "Grabbing DELUGE_LOGLEVEL=${DELUGE_LOGLEVEL}..."
+echo "Grabbing TZ=${TZ}..."
 
 # Change IDs of xyz user and group
-echo "Setting PUID=${PUID} and PGID=${PGID} to xyz user and group" | tee -a $LOG
-groupmod -o -g "$PGID" xyz | tee -a $LOG
-usermod -o -u "$PUID" xyz | tee -a $LOG
+echo "Setting PUID=${PUID} and PGID=${PGID} to xyz user and group"
+groupmod -o -g "$PGID" xyz
+usermod -o -u "$PUID" xyz
 
 # Setting folder ownership
-echo "Setting container volumes ownership to user xyz" | tee -a $LOG
+echo "Setting container volumes ownership to user xyz"
 chown xyz:xyz /config
 chown xyz:xyz /downloads
 chown xyz:xyz /watchfolder
 chown xyz:xyz /data
 
-echo "All done! Starting the services via supervisord..." | tee -a $LOG
-/usr/bin/supervisord | tee -a $LOG
+echo "All done! Starting the services via supervisord..."
+/usr/bin/supervisord
 
 exit 0
