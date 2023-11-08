f40a3dc55bf78f04019a57b67d4c9f93eee31c6c
Honey Lover <38346023+GentleHoneyLover@users.noreply.github.com>
2023-11-08 15:09:04 +0100

    Update init.sh

--- rootfs/opt/init.sh
+++ rootfs/opt/init.sh
@@ -8,6 +8,11 @@ echo "Grabbing UMASK=${UMASK}..."
 echo "Grabbing DELUGE_LOGLEVEL=${DELUGE_LOGLEVEL}..."
 echo "Grabbing TZ=${TZ}..."
 
+readonly PYTHON_VER=$(python3 -V | cut -f2 -d " " | cut -c 1-4)
+echo "Grabbing PYTHON_VER=${PYTHON_VER}..."
+readonly PLUGIN_NAME=$(ls /defaults/plugins | cut -c 1-20)
+echo "Grabbing PLUGIN_NAME=${PLUGIN_NAME}..."
+
 # Create xyz user and group
 echo "Setting PUID=${PUID} and PGID=${PGID} to xyz user and group"
 adduser --system --disabled-password --no-create-home --uid "$PUID" xyz
@@ -15,15 +20,24 @@ addgroup --system --gid "$PGID" xyz
 adduser xyz xyz
 
 # Handling default configs
-echo "Handling default config for Deluge"
+echo "Handling default config for Deluge..."
 if [[ ! -f $DELUGE_CONFIG/core.conf ]]; then
-    cp /defaults/core.conf $DELUGE_CONFIG/core.conf
+	cp /defaults/core.conf $DELUGE_CONFIG/core.conf
 fi
-echo "Installing the filebot plugin if it's missing"
-if [[ ! -f $DELUGE_CONFIG/plugins/FileBotTool-$FILEBOT_PLUGIN_VERSION.egg ]]; then
-    readonly PYTHON_VER=$(python3 -V | cut -f2 -d " " | cut -c 1-4)
-		cp "/defaults/plugins/FileBotTool-$FILEBOT_PLUGIN_VERSION.egg" "$DELUGE_CONFIG/plugins/FileBotTool-2.0.3-py${PYTHON_VER}.egg"
+echo "Installing the FileBot plugin if it's missing..."
+if [[ ! -f $DELUGE_CONFIG/plugins/FileBotTool-*$PYTHON_VER.egg ]]; then
+	echo "FileBot plugin is missing... Installing..."
+	rm $DELUGE_CONFIG/plugins/FileBotTool-*.egg
+	cp /defaults/plugins/FileBotTool-*.egg $DELUGE_CONFIG/plugins/$PLUGIN_NAME$PYTHON_VER.egg
 fi
+
+# Setting folder ownership
+echo "Setting container volumes ownership to user xyz"
+chown -R xyz:xyz /config
+chown -R xyz:xyz /downloads
+chown -R xyz:xyz /watchfolder
+chown -R xyz:xyz /data
+chown -R xyz:xyz /opt/filebot
 
 # Setting folder ownership
 echo "Setting container volumes ownership to user xyz"
