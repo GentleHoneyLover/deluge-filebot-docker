6bd7d2d5bccdf8259860f28db6c07d3889997b0c
GentleHoneyLover <38346023+GentleHoneyLover@users.noreply.github.com>
2023-11-15 21:37:33 +0100

    README + init script updates

--- rootfs/opt/init.sh
+++ rootfs/opt/init.sh
@@ -7,7 +7,11 @@ echo "Grabbing PUID=${PUID} and PGID=${PGID}..."
 echo "Grabbing UMASK=${UMASK}..."
 echo "Grabbing DELUGE_LOGLEVEL=${DELUGE_LOGLEVEL}..."
 echo "Grabbing TZ=${TZ}..."
+
+#readonly PYTHON_VER=$(python3 -V | cut -f2 -d " " | cut -f1,2 -d ".")
 echo "Grabbing PYTHON_VER=${PYTHON_VER}..."
+readonly PLUGIN_VER=$(ls /defaults/plugins | cut -f2 -d "-")
+echo "Grabbing PLUGIN_VER=${PLUGIN_VER}..."
 
 # Change IDs of xyz user and group
 echo "Setting PUID=${PUID} and PGID=${PGID} to xyz user and group"
@@ -19,15 +23,18 @@ echo "Handling default config for Deluge..."
 if [[ ! -f $DELUGE_CONFIG/core.conf ]]; then
 	cp /defaults/*.conf $DELUGE_CONFIG/
 fi
+
 echo "Installing the FileBot plugin if it's missing..."
 if [[ ! -f $DELUGE_CONFIG/plugins/FileBotTool-*$PYTHON_VER.egg ]]; then
 	echo "FileBot plugin is missing... Installing..."
 	if [[ ! -f $DELUGE_CONFIG/plugins/FileBotTool-*.egg ]]; then
+		echo "Creating plugins folder..."
 		mkdir -p $DELUGE_CONFIG/plugins
 	else
+		echo "Removing old plugins..."
 		rm $DELUGE_CONFIG/plugins/FileBotTool-*.egg
 	fi
-	cp /defaults/plugins/FileBotTool-*.egg $DELUGE_CONFIG/plugins/
+	cp /defaults/plugins/FileBotTool-*.egg $DELUGE_CONFIG/plugins/FileBotTool-$PLUGIN_VER-py$PYTHON_VER.egg
 fi
 
 # Setting folder ownership
